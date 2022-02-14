5b85aa4203872883d0e289553576fccbfab914ef
Asger Askov Blekinge <abr@kb.dk>
2022-02-14 15:05:18 +0100

    Fixed json error

--- AvisScanQA_web/src/main/java/dk/kb/kula190/webservice/Application.java
+++ AvisScanQA_web/src/main/java/dk/kb/kula190/webservice/Application.java
@@ -1,5 +1,8 @@
 package dk.kb.kula190.webservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import dk.kb.kula190.api.impl.DefaultApiServiceImpl;
 
@@ -12,7 +15,26 @@ import java.util.Set;
 public class Application extends javax.ws.rs.core.Application {
 
     public Set<Class<?>> getClasses() {
-        return new HashSet<>(Arrays.asList(JacksonJsonProvider.class, DefaultApiServiceImpl.class));
+        return new HashSet<>(Arrays.asList(DefaultApiServiceImpl.class, ServiceExceptionMapper.class));
     }
+    
+    
+    @Override
+    public Set<Object> getSingletons() {
+        return Set.of(getJsonProviderWithDateTimes());
+    }
+    
+    
+    public static JacksonJaxbJsonProvider getJsonProviderWithDateTimes() {
+        // see https://github.com/FasterXML/jackson-modules-java8
+        ObjectMapper mapper = new ObjectMapper();
+        
+        mapper.findAndRegisterModules();
+        mapper.disable(SerializationFeature.INDENT_OUTPUT);
+        
+        return new JacksonJaxbJsonProvider(mapper,
+                                           JacksonJaxbJsonProvider.DEFAULT_ANNOTATIONS);
+    }
+    
 }
 
