b57dd86d576c97ba77c19a2f18db23b47d603d12
Asger Askov Blekinge <abr@kb.dk>
2022-02-15 15:51:25 +0100

    Alto Checker

---
+++ AvisScanQA_common/src/main/java/dk/kb/kula190/checkers/singlecheckers/XpathAltoChecker.java
@@ -0,0 +1,63 @@
+package dk.kb.kula190.checkers.singlecheckers;
+
+import dk.kb.kula190.ResultCollector;
+import dk.kb.kula190.iterators.common.AttributeParsingEvent;
+import dk.kb.kula190.iterators.eventhandlers.decorating.DecoratedEventHandlerWithSections;
+import dk.kb.util.xml.XML;
+import dk.kb.util.xml.XPathSelector;
+import dk.kb.util.xml.XpathUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDate;
+
+public class XpathAltoChecker extends DecoratedEventHandlerWithSections {
+    public XpathAltoChecker(ResultCollector resultCollector) {
+        super(resultCollector);
+    }
+    
+    
+    @Override
+    public void altoFile(AttributeParsingEvent event,
+                         String avis,
+                         LocalDate editionDate,
+                         String udgave,
+                         String sectionName,
+                         Integer pageNumber) throws IOException {
+        Document document;
+        try (InputStream in = event.getData()) {
+            document = XML.fromXML(in, true);
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
+            throw new IOException(e);
+        }
+        XPathSelector xpath = XpathUtils.createXPathSelector("alto", "http://www.loc.gov/standards/alto/ns-v2#");
+        
+        
+        //        <Page ID="P1" HEIGHT="13835" WIDTH="11005" PHYSICAL_IMG_NR="1" QUALITY="OK" POSITION="Single" PROCESSING="OCR1"
+        //              ACCURACY="41.00" PC="0.410">
+        
+        Node pageNode = xpath.selectNode(document, "/alto:alto/alto:Layout/alto:Page");
+        
+        //TODO should this check actually be there?
+        checkAtLeast(event,
+                     "INVALID_ALTO",
+                     Double.parseDouble(pageNode.getAttributes().getNamedItem("ACCURACY").getNodeValue()),
+                     10.0,
+                     "ALTO OCR accurary {actual} is lower than {required}");
+        
+        checkEquals(event,
+                    "INVALID_ALTO",
+                    pageNode.getAttributes().getNamedItem("QUALITY").getNodeValue(),
+                    "OK",
+                    "ALTO quality should have been {expected} but was {actual}");
+        //TODO compare against acceptable levels
+        
+    }
+    
+}
