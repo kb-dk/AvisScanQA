2f7ce8dc356d256f301ca43bbade3c367a2d9b9b
Asger Askov Blekinge <abr@kb.dk>
2022-02-24 18:03:36 +0100

    Full CLI

--- AvisScanQA_cli/src/main/java/dk/kb/kula190/cli/Main.java
+++ AvisScanQA_cli/src/main/java/dk/kb/kula190/cli/Main.java
@@ -1,14 +1,25 @@
 package dk.kb.kula190.cli;
 
 import dk.kb.kula190.Batch;
+import dk.kb.kula190.MultiThreadedRunnableComponent;
 import dk.kb.kula190.ResultCollector;
 import dk.kb.kula190.RunnableComponent;
 import dk.kb.kula190.checkers.ChecksumChecker;
+import dk.kb.kula190.checkers.DatabaseRegister;
 import dk.kb.kula190.checkers.crosscheckers.NoMissingMiddlePagesChecker;
 import dk.kb.kula190.checkers.crosscheckers.PageStructureChecker;
+import dk.kb.kula190.checkers.crosscheckers.XpathCrossChecker;
+import dk.kb.kula190.checkers.singlecheckers.TiffAnalyzer;
+import dk.kb.kula190.checkers.singlecheckers.TiffChecker;
 import dk.kb.kula190.checkers.singlecheckers.XmlSchemaChecker;
+import dk.kb.kula190.checkers.singlecheckers.XpathAltoChecker;
+import dk.kb.kula190.checkers.singlecheckers.XpathMixChecker;
+import dk.kb.kula190.generated.Failure;
 import dk.kb.kula190.iterators.eventhandlers.TreeEventHandler;
+import dk.kb.util.yaml.YAML;
+import org.postgresql.Driver;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -16,14 +27,24 @@ public class Main {
     
     public static void main(String[] args) throws Exception {
     
-        RunnableComponent component = new RunnableComponent() {
+        RunnableComponent component = new MultiThreadedRunnableComponent() {
             @Override
             protected List<TreeEventHandler> getCheckers(ResultCollector resultCollector) {
                 return List.of(
-                        new ChecksumChecker(resultCollector),
+                        //Simple Checkers
+//                        new ChecksumChecker(resultCollector),
+        
+                        //Per file- checkers
+                        new XmlSchemaChecker(resultCollector),
+                        new TiffAnalyzer(resultCollector),
+                        new TiffChecker(resultCollector),
+                        new XpathAltoChecker(resultCollector),
+                        new XpathMixChecker(resultCollector),
+        
+                        //CrossCheckers
+                        new XpathCrossChecker(resultCollector),
                         new NoMissingMiddlePagesChecker(resultCollector),
-                        new PageStructureChecker(resultCollector),
-                        new XmlSchemaChecker(resultCollector));
+                        new PageStructureChecker(resultCollector));
             }
         };
     
@@ -31,7 +52,40 @@ public class Main {
         Batch batch = new Batch(batchPath.getFileName().toString(), batchPath);
         ResultCollector resultCollector = component.doWorkOnItem(batch);
     
+    
         System.out.println(resultCollector.toReport());
+        
+        
+        
+        final List<Failure> failures = resultCollector.getFailures();
+        File configFolder = new File(Thread.currentThread()
+                                           .getContextClassLoader()
+                                           .getResource("dbconfig-behaviour.yaml")
+                                           .toURI()).getParentFile();
+        YAML dbConfig = YAML.resolveLayeredConfigs(configFolder.getAbsolutePath() + "/dbconfig-*.yaml");
+    
+    
+        RunnableComponent databaseComponent = new RunnableComponent() {
+            @Override
+            protected List<TreeEventHandler> getCheckers(ResultCollector resultCollector) {
+                return List.of(
+                    
+                        new DatabaseRegister(resultCollector,
+                                             new Driver(),
+                                             dbConfig.getString("jdbc.jdbc-connection-string"),
+                                             dbConfig.getString("jdbc.jdbc-user"),
+                                             dbConfig.getString("jdbc.jdbc-password"),
+                                             failures)
+                              );
+            }
+        
+        };
+        ResultCollector dbResultCollector = databaseComponent.doWorkOnItem(batch);
+    
+    
+        System.out.println(dbResultCollector.toReport());
+        
+        System.exit(0);
     }
     
 
