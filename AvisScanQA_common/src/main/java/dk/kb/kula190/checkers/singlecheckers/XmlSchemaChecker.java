1302213e75baf6216dabb65ef254e18a62b60bb8
Asger Askov Blekinge <abr@kb.dk>
2022-03-15 10:57:57 +0100

    better xml schema checker

--- AvisScanQA_common/src/main/java/dk/kb/kula190/checkers/singlecheckers/XmlSchemaChecker.java
+++ AvisScanQA_common/src/main/java/dk/kb/kula190/checkers/singlecheckers/XmlSchemaChecker.java
@@ -2,9 +2,7 @@ package dk.kb.kula190.checkers.singlecheckers;
 
 import dk.kb.kula190.ResultCollector;
 import dk.kb.kula190.generated.FailureType;
-import dk.kb.kula190.iterators.common.AttributeParsingEvent;
 import dk.kb.kula190.iterators.common.ParsingEvent;
-import dk.kb.kula190.iterators.eventhandlers.EventRunner;
 import dk.kb.kula190.iterators.eventhandlers.decorating.DecoratedAttributeParsingEvent;
 import dk.kb.kula190.iterators.eventhandlers.decorating.DecoratedEventHandler;
 import dk.kb.kula190.iterators.eventhandlers.decorating.DecoratedNodeParsingEvent;
@@ -12,6 +10,7 @@ import org.slf4j.Logger;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
+
 import javax.xml.XMLConstants;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
@@ -25,101 +24,130 @@ import java.util.stream.Collectors;
 
 public class XmlSchemaChecker extends DecoratedEventHandler {
     private static Logger log = org.slf4j.LoggerFactory.getLogger(XmlSchemaChecker.class);
-    public Validator[] validators = {null,null};
-    private ParsingEvent parsingEvent;
+    private Validator mixValidator;
+    private Validator altoValidator;
+    
+    private ThreadLocal<ParsingEvent> parsingEvent = new ThreadLocal<>();
+    
     public XmlSchemaChecker(ResultCollector resultCollector) {
         super(resultCollector);
     }
-
+    
     @Override
-    public void batchBegins(DecoratedNodeParsingEvent event, String batch, String roundTrip, LocalDate startDate, LocalDate endDate) {
-        try (InputStream schemaStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("MetadataSchema/MixSchema.xsd");
-             InputStream schemaStream2 = Thread.currentThread().getContextClassLoader().getResourceAsStream("MetadataSchema/AltoSchema.xsd")) {
-            setErrorHandler("Mix",schemaStream, 0);
-            setErrorHandler("Alto",schemaStream2,1);
-
-
+    public void batchBegins(DecoratedNodeParsingEvent event,
+                            String batch,
+                            String roundTrip,
+                            LocalDate startDate,
+                            LocalDate endDate) {
+        try (InputStream schemaStream = Thread.currentThread()
+                                              .getContextClassLoader()
+                                              .getResourceAsStream("MetadataSchema/AltoSchema.xsd")) {
+            altoValidator = setErrorHandler("Alto", schemaStream);
         } catch (IOException | SAXException e) {
-            addFailure(event,
-                    FailureType.EXCEPTION,
-                    this.getClass().getSimpleName(),
-                       FailureType.UNEXPECTED_ERROR.name() +"\n"+ e,
-                    Arrays.stream(e.getStackTrace())
-                            .map(StackTraceElement::toString)
-                            .collect(Collectors.joining("\n")));
+            addExceptionalFailure(event, e);
         }
-
+        
+        try (InputStream schemaStream = Thread.currentThread()
+                                              .getContextClassLoader()
+                                              .getResourceAsStream("MetadataSchema/MixSchema.xsd")) {
+            mixValidator = setErrorHandler("Alto", schemaStream);
+        } catch (IOException | SAXException e) {
+            addExceptionalFailure(event, e);
+        }
+        
     }
-
-
-
+    
+    private void addExceptionalFailure(ParsingEvent event, Exception e) {
+        addFailure(event,
+                   FailureType.EXCEPTION,
+                   this.getClass().getSimpleName(),
+                   FailureType.UNEXPECTED_ERROR.name() + "\n" + e,
+                   Arrays.stream(e.getStackTrace())
+                         .map(StackTraceElement::toString)
+                         .collect(Collectors.joining("\n")));
+    }
+    
     @Override
-    public synchronized void mixFile(DecoratedAttributeParsingEvent event, String editionName, LocalDate editionDate, String
-            udgave, String sectionName, Integer pageNumber) throws IOException {
-        parsingEvent = event;
-        try (InputStream stream = event.getData()) {
-
-            validators[0].validate(new StreamSource(stream));
-        } catch (SAXException e) {
-            //Something wrong with the xml file - dump into resultcollector
-            log.error("SAX error", e);
-        } catch (IOException e) {
-            // handle exception while reading source
-            log.error("IO error", e);
+    public synchronized void injectedFile(DecoratedAttributeParsingEvent decoratedEvent,
+                             String injectedType,
+                             String avis,
+                             LocalDate editionDate,
+                             String udgave,
+                             String sectionName,
+                             Integer pageNumber) throws IOException {
+        
+        switch (injectedType) {
+            
+            case MetsSplitter.INJECTED_TYPE_MIX -> { //MIX FROM METS FILE
+                parsingEvent.set(decoratedEvent);
+                try (InputStream stream = decoratedEvent.getData()) {
+                    mixValidator.validate(new StreamSource(stream));
+                } catch (SAXException e) {
+                    addExceptionalFailure(parsingEvent.get(), e);
+                }
+            }
         }
-
-
     }
-
+    
     @Override
-    public synchronized void altoFile(DecoratedAttributeParsingEvent event, String editionName, LocalDate editionDate, String udgave, String sectionName, Integer pageNumber) throws IOException {
-        parsingEvent = event;
-        try (InputStream stream = event.getData()) {
-
-            validators[1].validate(new StreamSource(stream));
+    public synchronized void mixFile(DecoratedAttributeParsingEvent decoratedEvent,
+                                     String editionName,
+                                     LocalDate editionDate,
+                                     String udgave,
+                                     String sectionName,
+                                     Integer pageNumber) throws IOException {
+        parsingEvent.set(decoratedEvent);
+        try (InputStream stream = decoratedEvent.getData()) {
+            
+            mixValidator.validate(new StreamSource(stream));
         } catch (SAXException e) {
-            //Something wrong with the xml file - dump into resultcollector
-            log.error("SAX error", e);
-        } catch (IOException e) {
-            // handle exception while reading source
-            log.error("IO error", e);
+            addExceptionalFailure(parsingEvent.get(), e);
         }
     }
-
-    public void setErrorHandler(String name, InputStream stream, int validatorIndex) throws SAXException {
-
+    
+    @Override
+    public synchronized void altoFile(DecoratedAttributeParsingEvent event,
+                                      String editionName,
+                                      LocalDate editionDate,
+                                      String udgave,
+                                      String sectionName,
+                                      Integer pageNumber) throws IOException {
+        parsingEvent.set(event);
+        try (InputStream stream = event.getData()) {
+            altoValidator.validate(new StreamSource(stream));
+        } catch (SAXException e) {
+            addExceptionalFailure(parsingEvent.get(), e);
+        }
+    }
+    
+    public Validator setErrorHandler(String name, InputStream stream) throws SAXException {
+        
         SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         Schema schema = factory.newSchema(new StreamSource(stream));
-
-        validators[validatorIndex] = schema.newValidator();
-        validators[validatorIndex].setErrorHandler(new ErrorHandler() {
+        
+        Validator validator = schema.newValidator();
+        validator.setErrorHandler(new ErrorHandler() {
             @Override
             public void warning(SAXParseException exception) throws SAXException {
-                log.warn(name+" warning", exception);
+                log.warn(name + " warning", exception);
             }
-
+            
             @Override
             public void error(SAXParseException exception) throws SAXException {
-                addFailure(parsingEvent,
+                addFailure(parsingEvent.get(),
                            FailureType.SCHEMA_ERROR,
-                        XmlSchemaChecker.class.getSimpleName(),
-                        name+" error encountered",
-                        exception.getMessage());
+                           XmlSchemaChecker.class.getSimpleName(),
+                           name + " error encountered",
+                           exception.getMessage());
             }
-
+            
             @Override
             public void fatalError(SAXParseException exception) throws SAXException {
-                addFailure(parsingEvent,
-                           FailureType.EXCEPTION,
-                           this.getClass().getSimpleName(),
-                           FailureType.UNEXPECTED_ERROR.name() +"\n"+ exception,
-                           Arrays.stream(exception.getStackTrace())
-                                .map(StackTraceElement::toString)
-                                .collect(Collectors.joining("\n")));
-
+              addExceptionalFailure(parsingEvent.get(), exception);
             }
         });
+        return validator;
     }
-
-
+    
+    
 }
