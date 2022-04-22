123b0155e188192bd32e83a1159966c7e2b24447
Patrick Bruun <pabr@kb.dk>
2022-04-22 13:26:24 +0200

    Removed redundant line.

--- AvisScanQA_cli/src/main/java/dk/kb/kula190/cli/AvisScanQATool.java
+++ AvisScanQA_cli/src/main/java/dk/kb/kula190/cli/AvisScanQATool.java
@@ -37,55 +37,55 @@ import java.util.concurrent.Executors;
 import java.util.function.Function;
 
 public class AvisScanQATool {
-    
+
     private final Logger log = LoggerFactory.getLogger(getClass());
-    
+
     private final YAML config;
-    
+
     private final String checksumFile;
     private final String acknowledgmentFile;
     private final List<String> filesToIgnore;
-    
+
     public AvisScanQATool(YAML config, String checksumFile, String acknowledgmentFile, List<String> filesToIgnore) {
-        this.config             = config;
-        this.checksumFile       = checksumFile;
+        this.config = config;
+        this.checksumFile = checksumFile;
         this.acknowledgmentFile = acknowledgmentFile;
-        this.filesToIgnore      = filesToIgnore;
+        this.filesToIgnore = filesToIgnore;
     }
-    
+
     public ResultCollector check(Path batchPath) throws IOException {
-        
+
         Batch batch = new Batch(batchPath.getFileName().toString(), batchPath);
-        
+
         ResultCollector resultCollector = new ResultCollector(getClass().getSimpleName(),
                                                               getClass().getPackage().getImplementationVersion(),
                                                               null);
-        
+
         try {
             log.info("Initial filenameChecking of batch {}", batch.getFullID());
             //Perform basic checks
             final BasicRunnableComponent
                     basicRunnableComponent
                     = new BasicRunnableComponent(r -> List.of(new FileNamingChecker(r)), checksumFile, filesToIgnore);
-            
+
             basicRunnableComponent.doWorkOnItem(batch, resultCollector);
-            
+
         } catch (Exception e) {
             resultCollector.addExceptionalFailure(e);
         }
-        
+
         //If the basic checks worked, proceed
         if (resultCollector.isSuccess()) {
             try {
                 final int nThreads = config.getInteger("iterator.numThreads",
                                                        Runtime.getRuntime().availableProcessors());
                 log.info("Starting full checks of batch {} with {} threads", batch.getFullID(), nThreads);
-                
+
                 DecoratedRunnableComponent component = new MultiThreadedRunnableComponent(Executors.newFixedThreadPool(
                         nThreads), checkerFactory(), checksumFile, filesToIgnore);
-                
+
                 component.doWorkOnItem(batch, resultCollector);
-                
+
             } catch (Exception e) {
                 resultCollector.addExceptionalFailure(e);
             }
@@ -95,8 +95,7 @@ public class AvisScanQATool {
         if (config.getBoolean("jdbc.enabled")) {
             log.info("Registering results of QA on batch {} in database", batch.getFullID());
             registerResultInDB(batch, resultCollector, config);
-    
-config.get("mail");
+
             Properties emailConfig;
 
             emailConfig = dk.kb.util.yaml.YAMLUtils.toProperties(config.getSubMap("mail.smtp", true));
@@ -106,21 +105,22 @@ config.get("mail");
                        .cc(config.getString("mail.cc"))
                        .bcc(config.getString("mail.bcc"))
                        .subject(config.getString("mail.subject"))
-                       .bodyText(config.getString("mail.bodyText")+" <a href='"+config.getString("mail.URL")+batch+"'>"+batch+"</a>",
+                       .bodyText(config.getString("mail.bodyText") +
+                                 " <a href='" + config.getString("mail.URL") + batch + "'>" + batch + "</a>",
                                  "text/html")
                        //.attachment(Path.of(config.getString("mail.attachment")))
                        .send(emailConfig);
 
         }
-        
+
         log.info("All checks done, returning");
         return resultCollector;
     }
-    
-    
+
+
     private void registerResultInDB(Batch batch, ResultCollector resultCollector, YAML config) throws IOException {
         final List<Failure> failures = resultCollector.getFailures();
-        
+
         DecoratedRunnableComponent databaseComponent = new DecoratedRunnableComponent(
                 rc -> List.of(new DatabaseRegister(rc,
                                                    new Driver(),
@@ -134,32 +134,32 @@ config.get("mail");
                 checksumFile,
                 filesToIgnore);
         databaseComponent.doWorkOnItem(batch, resultCollector);
-        
+
     }
-    
+
     private Function<ResultCollector, List<TreeEventHandler>> checkerFactory() {
         //TODO ensure this is the current and correct list of checkers
         return resultCollector -> List.of(
-                
+
                 //BatchCheckers
                 new MetsSplitter(resultCollector), new MetsChecker(resultCollector),
-                
+
                 //EditionCheckers
                 new NoMissingMiddlePagesChecker(resultCollector),
-                
+
                 //PageCheckers
                 new XpathPageChecker(resultCollector), new PageStructureChecker(resultCollector),
-                
+
                 //FileCheckers
                 new ChecksumChecker(resultCollector), new XmlSchemaChecker(resultCollector),
-                
+
                 //Tiff Checkers - Exiv2
                 new TiffAnalyzerExiv2(resultCollector), new TiffCheckerExiv2(resultCollector),
-                
+
                 //Tiff Checkers - ImageMagick
                 new TiffAnalyzerImageMagick(resultCollector), new TiffCheckerImageMagick(resultCollector),
-                
+
                 new ProgressLogger(resultCollector));
     }
-    
+
 }
