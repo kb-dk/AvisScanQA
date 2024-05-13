5e04081f31d0d33782ffbcb180bb88ed19752191
Patrick Bruun <pabr@kb.dk>
2024-05-13 10:26:09 +0200

    updated postgresql jdbc version to latest

--- AvisScanQA_cli/src/test/java/dk/kb/kula190/cli/MainInvoker.java
+++ AvisScanQA_cli/src/test/java/dk/kb/kula190/cli/MainInvoker.java
@@ -17,7 +17,7 @@ public class MainInvoker {
 //            = new File(System.getenv("HOME") + "/Documents/ribesocialdemokrat_19100101_19100531_RT2");
     private static final File
             specificBatch
-            = new File(System.getenv("HOME") + "/Documents/TestBatches/kioebenhavnsaftenpost1772_17730101_17731231_RT1/");
+            = new File(System.getenv("HOME") + "/Documents/TestBatches/dendanskeslesviger_18550101_18551231_RT3/");
 //    private static final File
 //            specificBatch
 //            = new File(System.getenv("HOME") + "/Documents/this is a + test folder/schleswigschegrenzpost_18650103_18700306_RT2");
